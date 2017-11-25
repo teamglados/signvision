7f21b8078890c099340ef3aed9bb5e1b25e878ae
Teemu Taskula <teemu.taskula@aalto.fi>
2017-11-25 20:40:31 +0200

    Added redux-logger and some mapbox configs

--- client/config.js
+++ client/config.js
@@ -1,4 +1,6 @@
+/* eslint-disable max-len */
 import { Platform } from 'react-native';
 
 export const IS_ANDROID = Platform.OS === 'android';
 export const MAP_BOX_TOKEN = 'pk.eyJ1IjoidGVlbXV0YXNrdWxhIiwiYSI6ImNqYWYwano1MjB6eTYyd3F1OGNjMWczMGcifQ.KVj38ECZkRdLx06fMyBMRQ';
+export const MAP_BOX_API = 'https://api.mapbox.com/directions/v5/mapbox';
--- client/store.js
+++ client/store.js
@@ -1,6 +1,7 @@
 import { createStore, applyMiddleware, combineReducers } from 'redux';
 import createSagaMiddleware from 'redux-saga';
 import { fork } from 'redux-saga/effects';
+import logger from 'redux-logger';
 import markReducer, { markSagas } from './features/mark/mark.ducks';
 
 import createWsMiddleware from './middleware';
@@ -16,7 +17,7 @@ function* rootSaga() {
 // create the saga middleware
 const sagaMiddleware = createSagaMiddleware();
 const wsMiddleware = createWsMiddleware();
-const enhancer = applyMiddleware(sagaMiddleware, wsMiddleware);
+const enhancer = applyMiddleware(logger, sagaMiddleware, wsMiddleware);
 
 export default function configureStore(initialState) {
   const store = createStore(rootReducer, initialState, enhancer);
