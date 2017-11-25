6a28d2554bccc8e517b12cc51f69f8a7c0442c8d
Andreas <urbanski.andreas@gmail.com>
2017-11-25 20:34:50 +0200

    Auto-package source files

--- docker/build.sh
+++ docker/build.sh
@@ -1,4 +1,14 @@
+if [ -f ./server/pkg.tar.gz ]; then
+  rm ./server/pkg.tar.gz
+fi
+
+echo "Creating tar pkg..."
+pushd ..
+tar --exclude="node_modules" -zcvf docker/server/pkg.tar.gz ./server
+popd
+
 echo "Building tensorflow base image..."
 docker build --tag=glados:tf tf
+
 echo "Building server image..."
 docker build --tag=glados:server server
