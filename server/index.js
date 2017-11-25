0d485b8cc7fcf018e1ea4d4f85dc8dcdff99c26a
Andreas <urbanski.andreas@gmail.com>
2017-11-25 19:16:50 +0200

    Fix code formatting

--- server/index.js
+++ server/index.js
@@ -1,23 +1,21 @@
 const server = require('./server');
 
-// Collect api
+// Collect api and streams
 const { sendEvent } = server;
-
-// Collect streams
 const { connections, messages, errors, options } = server;
 
 // Subscribe to new connections
 connections.subscribe(([ws, chan]) => {
   console.log(`* new connection channel ${chan} ${ws._socket.remoteAddress}`);
-})
+});
 
 // Subscribe to messages
 messages.subscribe(([evt, chan]) => {
   console.log(`* new event on channel ${chan}`, evt);
   sendEvent(chan, 'event_server', 'foo');
-})
+});
 
 // TODO: Event handlers
 //
 
-console.log('>> ws server started on port', options.port);
+console.log('>> WS server started on port', options.port);
