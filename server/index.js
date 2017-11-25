31343252e4ae6d0fda6f6c46676fde5132153b89
Andreas <urbanski.andreas@gmail.com>
2017-11-25 19:10:02 +0200

    Restructure server logic

--- server/index.js
+++ server/index.js
@@ -1,15 +1,23 @@
-const { connections, messages, errors, port } = require('./server');
+const server = require('./server');
 
-console.log('>> ws server started on port', port);
+// Collect api
+const { sendEvent } = server;
 
-// Subscribe to connections
-connections.subscribe(x => {
-  console.log('new connection:', x._socket.remoteAddress);
-});
+// Collect streams
+const { connections, messages, errors, options } = server;
+
+// Subscribe to new connections
+connections.subscribe(([ws, chan]) => {
+  console.log(`* new connection channel ${chan} ${ws._socket.remoteAddress}`);
+})
 
 // Subscribe to messages
-messages.subscribe(x => {
-  console.log('new message:', x);
-});
+messages.subscribe(([evt, chan]) => {
+  console.log(`* new event on channel ${chan}`, evt);
+  sendEvent(chan, 'event_server', 'foo');
+})
 
 // TODO: Event handlers
+//
+
+console.log('>> ws server started on port', options.port);
--- server/server.js
+++ server/server.js
@@ -1,12 +1,23 @@
+// Require libs
 const { EventEmitter } = require('events');
 const uWs = require('uws');
 const Rx = require('rx');
 
+// Require utils
+const { createChannel, createPayload } = require('./utils');
+const dedupe = require('./dedupe');
+
 // Create options
 const { port = 3000 } = process.env;
 
+// Create options storage
+const options = { port };
+
+// Create channel storage
+const channels = {};
+
 // Create new ws server
-const wss = new uWs.Server({ port });
+const wss = new uWs.Server(options);
 
 // Create event emitter
 const wse = new EventEmitter();
@@ -16,20 +27,46 @@ const connections = Rx.Observable.fromEvent(wse, 'connection');
 const messages = Rx.Observable.fromEvent(wse, 'message');
 const errors = Rx.Observable.fromEvent(wse, 'error');
 
-// Attach connection, error listeners
-wss.on('connection', conn => wse.emit('connection', conn));
-wss.on('error', err => wse.emit('error', err));
+// High level send function
+const sendEvent = (chan, type, payload) => {
+  if (channels[chan]) {
+    channels[chan].send(JSON.stringify(createPayload({type, payload})));
+  }
+}
 
-// Subscribe to connections
-connections.subscribe(ws => {
-  ws.on('message', msg => wse.emit('message', JSON.parse(msg) || {}));
-  ws.on('error', err => wse.emit('error', err));
+// Attach listeners
+wss.on('connection', socket => {
+
+  // Create new channel and save socket
+  const chan = createChannel();
+  channels[chan] = socket;
+
+  // Acknowlege client channel
+  sendEvent(chan, 'channel', chan);
+
+  // Attach message listeners
+  socket.on('message', evt => {
+    evt = createPayload(evt);
+    if (Object.keys(channels).indexOf(evt.chan) > -1 && dedupe(evt)) {
+      wse.emit('message', [evt.data, evt.chan]);
+    }
+  });
+
+  // Attach socket error listeners
+  socket.on('error', err => {
+    wse.emit('error', err);
+  });
+
+  // Emit connection
+  wse.emit('connection', [socket, chan]);
 });
 
 // Export all
 module.exports = {
+  channels,
   connections,
   messages,
   errors,
-  port
+  options,
+  sendEvent,
 };
